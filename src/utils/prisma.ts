339e1e72a4373ab67b1e27582a0fa5ebf78d3cd4
EvgeniyT <evgeniy.titov@kilo.solutions>
2026-01-30 13:08:51 +0100

    feature(prisma) updated prisma util

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,21 +1,15 @@
 import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
-  prismaForAuth: PrismaClient;
 };
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
 
 const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  }).$extends(withAccelerate());
+  new PrismaClient({ accelerateUrl: process.env.DATABASE_URL as string }).$extends(
+    withAccelerate()
+  );
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
